a86bd8e5e44e8610a30319f7d1ed37faf33c7a96
David Hack <davidhack11@gmail.com>
2023-08-02 11:22:03 +1000

    Removed rounded border

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ export default function Layout({ children }) {
   return (
     <>
       <main className="h-full relative mb-8">
-        <div className="h-full rounded-t-lg border-t border-l border-r border-gray-400 px-3 py-10 bg-gray-200 flex justify-center">
+        <div className="h-full border-t border-l border-r border-gray-400 px-3 py-10 bg-gray-200 flex justify-center">
           {children}
         </div>
       </main>
